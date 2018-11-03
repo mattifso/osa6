7e841f684dcc6ee5f3bd2c79b04e163bf4cf37bf
Matti <mjsillan@iki.fi>
2018-11-03 12:30:14 +0200

    Add submission for assignment 6.4.

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { creation } from '../reducers/anecdoteReducer'
+import { notifyCreated, removeNotification } from '../reducers/notificationReducer'
 
 class AnecdoteForm extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault()
     const content = e.target.anecdote.value
     this.props.store.dispatch(creation(content))
+    this.props.store.dispatch(notifyCreated())
+    setTimeout(() => {
+      this.props.store.dispatch(removeNotification())
+    }, 5000)
     e.target.anecdote.value = ''
   }
   render() {
--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,5 +1,20 @@
-const reducer = (store = 'this is a notification', action) => {
+const reducer = (store = '', action) => {
+  if (action.type === 'NOTIFY_CREATED') {
+    return 'Created a a new anecdote'
+  }
+
+  if (action.type === 'REMOVE_NOTIFICATION') {
+    return ''
+  }
+
   return store
+}
+export const notifyCreated = () => {
+  return { type: 'NOTIFY_CREATED' }
+}
+
+export const removeNotification = () => {
+  return { type: 'REMOVE_NOTIFICATION' }
 }
 
 export default reducer
