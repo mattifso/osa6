46421b3fa306e3f551f015d83e693bc6e9a7350b
Matti <mjsillan@iki.fi>
2018-11-06 23:31:12 +0200

    Add submission for assignment 6.22, add picture of Douglas Crockford looking at your merge request.

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link, Redirect, NavLink } from 'react-router-dom'
-import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import { ListGroup, ListGroupItem, Grid, Col, Image } from 'react-bootstrap'
 
 const Menu = ({ anecdotes, addNew, anecdoteById, notify }) => {
   const menuStyle = {
@@ -57,12 +57,19 @@ const About = () => (
     <h2>About anecdote app</h2>
     <p>According to Wikipedia:</p>
 
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-        Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-        such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+    <Grid>
+      <Col sm={8}>
+        <em>An anecdote is a brief, revealing account of an individual person or an incident.
+            Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+            such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+          An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+        <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+      </Col>
+      <Col sm={2}>
+        <Image src="https://static.frontendmasters.com/assets/teachers/crockford/thumb@2x.jpg" />
+      </Col>
+    </Grid>
   </div>
 )
 
@@ -192,7 +199,7 @@ class App extends React.Component {
     }
 
     return (
-      <div>
+      <div className="container">
         <Router>
           <div>
             <h1>Software anecdotes</h1>
